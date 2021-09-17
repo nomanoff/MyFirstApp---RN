4d9f7b8915c3f7ab3f7d3480eff6dfe273f030b1
Azizbek Nomanoff <nomanoff@nomanoffs-mac.local>
2021-09-17 14:26:38 +0900

    flex / orientations

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Dimensions,
+  Image,
+  TouchableWithoutFeedback,
+  Button,
+  Alert,
+} from "react-native";
+import {
+  useDeviceOrientation,
+  useDimensions,
+} from "@react-native-community/hooks";
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>This is my first app.</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "crimson",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "blueviolet",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "bisque",
+          width: 100,
+          height: 100,
+        }}
+      />
     </View>
   );
 }
@@ -16,7 +55,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
