1ea511b54ef690b75ca2f3ada0c1bc70fb2753fd
Azizbek Nomanoff <nomanoff@nomanoffs-mac.local>
2021-09-19 00:41:14 +0900

    messages screen is fixed

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 import ListItem from "../components/ListItem";
@@ -6,7 +6,7 @@ import ListItemSeperator from "../components/ListItemSeperator";
 import ListItemDeleteAction from "../components/ListItemDeleteAction";
 import Screen from "../components/Screen";
 
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: "Title 1",
@@ -28,6 +28,14 @@ const messages = [
 ];
 
 function MessagesScreen(props) {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message) => {
+    //Delete the message from
+    const newMessages = messages.filter((m) => m.id !== message.id);
+    setMessages(newMessages);
+  };
   return (
     <Screen>
       <FlatList
@@ -40,13 +48,34 @@ function MessagesScreen(props) {
             image={item.image}
             onPress={() => console.log("Selected:", item.id)}
             renderRightActions={() => (
-              <ListItemDeleteAction
-                onPress={() => console.log("Delete", item)}
-              />
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
             )}
           />
         )}
         ItemSeparatorComponent={ListItemSeperator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 1,
+              title: "Title 1",
+              description: "Description 1",
+              image: require("../assets/new_profile.jpg"),
+            },
+            {
+              id: 2,
+              title: "Title 2",
+              description: "Description 2",
+              image: require("../assets/new_profile.jpg"),
+            },
+            {
+              id: 3,
+              title: "Title 3",
+              description: "Description 3",
+              image: require("../assets/new_profile.jpg"),
+            },
+          ]);
+        }}
       />
     </Screen>
   );
